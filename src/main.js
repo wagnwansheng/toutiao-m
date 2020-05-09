95f2ea74f0f535192da85954557892bb2c7e9cf8
Wangwansheng <1071919611@qq.com>
2020-05-09 10:49:30 +0800

    完成安装加载vant组件库

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载全局样式
+import './styles/index.less'
+// 加载vant组件库
+import Vant from 'vant'
+// 加载vant组件库样式
+import 'vant/lib/index.css'
 
+// 全局注册vant中组件
+Vue.use(Vant)
 Vue.config.productionTip = false
 
+// 创建 Vue 根实例   把router，store配置到根实例中
 new Vue({
   router,
   store,
