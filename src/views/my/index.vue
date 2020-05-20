b21d3c0fb3ac9b5079bd659d99890f889e435576
Wangwansheng <1071919611@qq.com>
2020-05-20 11:50:35 +0800

    我的页面布局完成

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,6 +1,67 @@
 <template>
   <div class="my-container">
-    我的
+      <van-cell
+        title=""
+        value=""
+        center
+      >
+        <van-image
+          slot="icon"
+          round
+          width="66.5px"
+          height="66.5px"
+          fit="cover"
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        />
+        <div slot="title">昵称</div>
+        <van-button
+          small
+          round
+        >编辑资料</van-button>
+      </van-cell>
+      <van-grid :column-num="4">
+        <van-grid-item>
+          <div class="span">123</div>
+          <div class="text">头条</div>
+        </van-grid-item>
+        <van-grid-item>
+          <div class="span">123</div>
+          <div class="text">关注</div>
+        </van-grid-item>
+        <van-grid-item>
+          <div class="span">123</div>
+          <div class="text">粉丝</div>
+        </van-grid-item>
+        <van-grid-item>
+          <div class="span">123</div>
+          <div class="text">获赞</div>
+        </van-grid-item>
+      </van-grid>
+      <van-grid :column-num="2">
+        <van-grid-item
+        icon-prefix="toutiao-m"
+        icon="shoucang"
+        text="收藏"
+        />
+        <van-grid-item
+        icon-prefix="toutiao-m"
+        icon="lishi"
+        text="历史"
+        />
+      </van-grid>
+      <van-cell
+      title="消息通知"
+      is-link
+      to="/"
+      />
+      <van-cell
+      title="小智同学"
+      is-link
+      to="/"
+      />
+      <van-cell
+      title="退出登录"
+      />
   </div>
 </template>
 
